449256dc26361a8eaaacecfb555c08709e7caf9c
damodara2006 <damodara2006@gmail.com>
2025-05-27 14:20:21 +0530

    same site

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -8,7 +8,7 @@ const login = async(req,res)=>{
     const value = jwt.sign({email, profile},"damo")
     // console.log(value)
     const verified = jwt.verify(value,"damo",{maxAge:60000})
-    res.cookie("login",value,{sameSite:"Strict"})
+    res.cookie("login",value,{sameSite:"none"})
     res.send({email,profile})
     } catch (error) {
        console.log(error) 
